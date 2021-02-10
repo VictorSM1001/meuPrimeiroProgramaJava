70f5b77ec18f91ec9ab93dbd91551b1ed2106eed
victo <victo@DESKTOP-IJNNBN7>
2021-02-10 09:23:03 -0300

    Alterações nos Comentários.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -18,11 +18,15 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// Saldação e pergunta o nome.
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		
+		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
+		//Saldação específica.
 		System.out.printf("Olá %s!\n", nome);
 	}
 
